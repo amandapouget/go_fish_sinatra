049fa37cb2a763c5408e4b80513b67b310b249b1
Amanda Simon <mandysimon88@gmail.com>
2015-10-23 09:44:35 -0400

    Create mock_client

---
+++ lib/mock_client.rb
@@ -0,0 +1,25 @@
+require 'socket'
+
+class MockClient
+  attr_reader :socket
+
+  def start
+    @socket = TCPSocket.open('localhost', 2000)
+  end
+
+  def provide_input(text)
+    @socket.puts(text)
+  end
+
+  def capture_output(delay=0.1)
+    sleep(delay)
+    @my_output = @socket.read_nonblock(1000)
+  rescue IO::WaitReadable
+    @my_output = ""
+  end
+
+  def output
+    capture_output
+    @my_output
+  end
+end
