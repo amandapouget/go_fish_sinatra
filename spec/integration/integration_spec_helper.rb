62cafe982e39da71aed03316de89a401dae54fdd
Amanda Simon <mandysimon88@gmail.com>
2015-11-09 18:45:40 -0500

    Use poltergeist for all rspec integration tests

--- spec/integration/integration_spec_helper.rb
+++ spec/integration/integration_spec_helper.rb
@@ -1,6 +1,7 @@
 require 'capybara/rspec'
-require 'selenium-webdriver'
 require './app'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false) # "After line 3 in your integration testing spec (Capybara.app = Sinatra::Application), add the line set(:show_exceptions, false) to ensure that when a test is passing there are no errors." What does this mean?
 
