3b3b758e14a24cea64100c3ea4eabf051363d4e1
Amanda Simon <mandysimon88@gmail.com>
2015-11-16 11:46:16 -0500

    Move features to spec folder

---
+++ spec/features/support/env.rb
@@ -0,0 +1,17 @@
+require '.././app'
+require 'rspec'
+require 'capybara'
+require 'factory_girl'
+require 'spinach/capybara'
+require './features/steps/helpers'
+require 'selenium-webdriver'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+Spinach.hooks.on_tag("javascript") { ::Capybara.current_driver = ::Capybara.javascript_driver }
+Spinach.config[:failure_exceptions] << RSpec::Expectations::ExpectationNotMetError
+Spinach::FeatureSteps.include RSpec::Matchers
+Spinach::FeatureSteps.include FactoryGirl::Syntax::Methods
+Spinach.hooks.before_run { FactoryGirl.reload }
